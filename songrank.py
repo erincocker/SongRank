6d95c5e9f11dcec2b0ea4db3d07c22bd5c212d19
erincocker <147055540+erincocker@users.noreply.github.com>
2023-10-11 17:30:34 +0100

    Update songrank.py
    Implemented saving/loading feature.
    Merged Songbook and Masterlist classes into one class (SongLists).
    Removed Song class and instead just use a dictionary for each song.
    Changed strategy for dealing with the first comparison - first way of doing it was silly

--- songrank.py
+++ songrank.py
@@ -1,212 +1,182 @@
 import csv
-from random import choice, sample
-
-#to do next:
-
-#include importing/exporting/saving functionality so user can stop
-# and later come back to the same list
-
-#change song picker so it picks optimally: pick the song halfway between
-# the upper and lower bounds
-
-#make everything neater and clearer - both on the ui side
-# and on the code side
+from random import choice, sample, randint
+import json
 
 
-class Song:
-    def __init__(self, id: int):
-        self.id = id
-
-        #song.compare[i] will be '+' if the song is better than song i
-        # '-' if song is worse than song i
-        # or '' if they haven't been compared
-        self.compare = []
-        for i in range(len(App.Songs)):
-            self.compare.append('')
-
-
-
-#songbook contains songs yet to be added to the master list
-#dictionary in the form dictionary[songid] = Song object corresponding to id
-#songs to be removed from songbook when they are added to the master list
-class SongBook:
+class SongLists:
     def __init__(self):
-        self.songs = {}
+        # initial list: songs yet to be added to master list
+        # though it's a dictionary so we can access songs by id
+        self.initial_list = {}
         for i in range(len(App.Songs)):
-            self.songs[i] = Song(i)
-    
+            self.initial_list[i] = []
+            for j in range(len(App.Songs)):
+                self.initial_list[i].append("")
+            # initial_list[i][j] will be '+' if song i is better than song j
+            # '-' if song i is worse than song j
+            # or '' if they haven't been compared
 
-    #user says which song is better
-    #info is saved to the song objects
-    #program checks if either song can go into the master list yet
-    def make_comparison(self, song_S: int, song_M: int, better: str):
-        if better == '1':
-            self.songs[song_S].compare[song_M] = '+'
-        elif better == '2':
-            self.songs[song_S].compare[song_M] = '-'
+        # master list: ranked songs. gradually add to this until completion
+        self.master_list = []
 
+    # save info from a comparison to the initial list
+    def make_comparison(self, song_I: int, song_M: int, better: str):
+        if better == "1":
+            self.initial_list[song_I][song_M] = "+"
+        elif better == "2":
+            self.initial_list[song_I][song_M] = "-"
 
-
-
-
-class MasterList:
-    def __init__(self):
-        self.songs = []
-
-    #returns the bounds for the song's possible position in the masterlist
-    #note upperbound should be < lowerbound as a smaller number means better position
-    #it is an error with my programming if upperbound > lowerbound
-    def find_bounds(self, songid: int, songbook: SongBook):
-        upperbound = 0 
-        lowerbound = len(self.songs)
-        for i in range(len(self.songs)):
-            comparison = songbook.songs[songid].compare[self.songs[i]]
-            if comparison == '-':
-                #i.e. if the song is worse than the song in the ith position in masterlist
-                upperbound = max(upperbound, i+1)
-            elif comparison == '+':
+    # return the bounds for the song's possible position in the masterlist
+    # note upperbound should be < lowerbound as a smaller number means better position
+    # it is an error with my programming if upperbound > lowerbound
+    def find_bounds(self, songid: int):
+        upperbound = 0
+        lowerbound = len(self.master_list)
+        for i in range(len(self.master_list)):
+            comparison = self.initial_list[songid][self.master_list[i]]
+            if comparison == "-":
+                # i.e. if the song is worse than the song in the ith position in masterlist
+                upperbound = max(upperbound, i + 1)
+            elif comparison == "+":
                 lowerbound = min(lowerbound, i)
-        
+
         return upperbound, lowerbound
 
-    #check if song1 can be added to masterlist
-    #if not, add any extra info to song1 
+    # check if song1 can be added to masterlist
+    # if not, add any extra info to song1
     # i.e. if songA better than songB also implies songA better than songC,
     # then add this info to songA
-    def check(self, songid: int, songbook: SongBook):
-        upperbound, lowerbound = self.find_bounds(songid, songbook)
+    def check(self, songid: int):
+        upperbound, lowerbound = self.find_bounds(songid)
 
         if upperbound > lowerbound:
-            raise ValueError('CONTRADICTION!')
-        
-        #if upper = lower, position in masterlist is determined
-        #place the song in masterlist and remove it from songbook
-        elif upperbound == lowerbound:
-            self.songs.insert(upperbound, songid)
-            songbook.songs.pop(songid)
+            raise ValueError("CONTRADICTION!")
 
-        #otherwise, add any extra inferred info to songbook and move on
+        # if upper = lower, position in masterlist is determined
+        # place the song in masterlist and remove it from initial list
+        elif upperbound == lowerbound:
+            self.master_list.insert(upperbound, songid)
+            self.initial_list.pop(songid)
+
+        # otherwise, add any extra inferred info to initial list and move on
         else:
             for i in range(0, upperbound):
-                songbook.songs[songid].compare[self.songs[i]] = '-'
-            for i in range(lowerbound+1, len(self.songs)):
-                songbook.songs[songid].compare[self.songs[i]] = '+'
+                self.initial_list[songid][self.master_list[i]] = "-"
+            for i in range(lowerbound + 1, len(self.master_list)):
+                self.initial_list[songid][self.master_list[i]] = "+"
 
+    # place one random song into the masterlist to begin with
+    def first_song(self):
+        songid = randint(0, len(App.Songs))
+        self.initial_list.pop(songid)
+        self.master_list.insert(0, songid)
 
+    # saved data is a json file
+    # the file contains a list containing the initial list and master list
+    def savedata(self, filename: str):
+        with open(filename, "w") as file:
+            file.write(json.dumps([self.initial_list, self.master_list]))
 
+    def importdata(self, filename: str):
+        with open(filename) as file:
+            [self.initial_list, self.master_list] = json.loads(file.read())
 
 
 class App:
-    Songs = [] #full list of songs and albums, not to be changed
-    
+    Songs = []  # full list of songs and albums, not to be changed
+
     def __init__(self):
         pass
 
     def get_songs(self, filename: str):
         with open(filename) as file:
-            for line in csv.reader(file, delimiter=','):
+            for line in csv.reader(file, delimiter=","):
                 if len(line) == 1:
                     App.Songs.append([line[0]])
                 elif len(line) == 2:
-                    App.Songs.append([line[0],line[1]])
+                    App.Songs.append([line[0], line[1]])
 
-
-    #want to choose one song from songbook then one from masterlist that
+    # want to choose one song from initial list then one from masterlist that
     # hasn't yet been compared to the first song
-    def choose_pair(self, songbook: SongBook, masterlist: MasterList):
-        song_S = choice(list(songbook.songs.keys()))
-        #second song must also be within the upper and lower bounds of song_S
-        #otherwise the comparison is redundant
-        upperbound, lowerbound = masterlist.find_bounds(song_S, songbook)
-        song_M = choice(masterlist.songs[upperbound:lowerbound])
-        return song_S, song_M
-    #returns id of the songs to be compared, song S from Songbook, song M from Masterlist
+    # returns id of the songs to be compared, song S from initial list, song M from Masterlist
+    def choose_pair(self, songlists: SongLists):
+        song_I = choice(list(songlists.initial_list.keys()))
+        # second song must also be within the upper and lower bounds of song_I
+        # otherwise the comparison is redundant
+        upperbound, lowerbound = songlists.find_bounds(song_I)
+        song_M = choice(songlists.master_list[upperbound:lowerbound])
+        return song_I, song_M
 
-
-    #for the first comparison as the first comparison must be between two songs from
-    # the songbook, as the masterlist starts as empty
-    def first_comparison(self, songbook: SongBook, masterlist: MasterList):
-        
-        song1, song2 = sample(list(songbook.songs.keys()), 2)
-        print('Which is better?')
-        print(f'Song 1: {App.Songs[song1][0]}')
-        print(f'Or Song 2: {App.Songs[song2][0]}')
-        better = input('1 or 2: ')
-        
-        if better == '1':
-            masterlist.songs.append(song1)
-            masterlist.songs.append(song2)
-        elif better == '2':  
-            masterlist.songs.append(song2)
-            masterlist.songs.append(song1)  
-        else:
-            print('...1 or 2 plz')
-            exit()#to be editsd
-
-        songbook.songs.pop(song1)
-        songbook.songs.pop(song2)
-        return songbook, masterlist
-
-
+    def load_data(self, songlists: SongLists):
+        filename = input("Enter filename of saved data: ")
+        songlists.importdata(filename)
+        songlists.initial_list = {
+            int(k): songlists.initial_list[k] for k in songlists.initial_list
+        }  # json files turn dictionary keys that are integers into strings
+        # must turn them back into integers
+        print()
+        return songlists
 
     def execute(self):
-        
-
-        print('''
+        print(
+            """
         Welcome to Song Ranker
               
         At each pair of songs, enter 1 to choose the first song,
         or 2 to choose the second
               
-        At any point after the first comparison, enter SongBook to see the songbook
-        or enter MasterList to see the list so far.
+        At any point, type in...
+        MasterList: to see the ranked list so far.
+        Save: to save and quit
+
               
-        ''')
-        
-        filename = input('First enter the file name: ')
-        self.get_songs(filename)
-        songbook = SongBook()
-        masterlist = MasterList()
+        """
+        )
 
-        if masterlist.songs == []:
-            songbook, masterlist = self.first_comparison(songbook, masterlist)
-            print()
-        #will later add in a command list, option to exit, option to save list so far, etc. 
+        # filename = input("First enter the file name: ")
+        self.get_songs("songs.txt")
+
+        songlists = SongLists()
+
+        if input("Load saved data? (y/n): ") == "y":
+            songlists = self.load_data(songlists)
+
+        if songlists.master_list == []:
+            songlists.first_song()
+
+        # will later add in a command list, option to exit, option to save list so far, etc.
         while True:
-           
-            #first get a pair of songs
-            song_S, song_M = self.choose_pair(songbook, masterlist)
-            #user chooses their favourite and the songbook is updated
-            # with the preference
-            print('Which is better?')
-            print(f'Song 1: {App.Songs[song_S][0]}')
-            print(f'Or Song 2: {App.Songs[song_M][0]}')
-            better = input('1 or 2: ')
-            if better == '1' or better == '2':
-                songbook.make_comparison(song_S, song_M, better)
-            elif better == 'SongBook':
+            # first get a pair of songs
+            song_I, song_M = self.choose_pair(songlists)
+            # user chooses their favourite and the initial list is updated
+            print("Which is better?")
+            print(f"Song 1: {App.Songs[song_I][0]}")
+            print(f"Or Song 2: {App.Songs[song_M][0]}")
+            better = input("1 or 2: ")
+
+            if better == "1" or better == "2":
+                songlists.make_comparison(song_I, song_M, better)
+            elif better == "MasterList":
                 print()
-                for i in songbook.songs:
-                    print(songbook.songs[i])
-            elif better == 'MasterList':
-                print()
-                for songid in masterlist.songs:
+                for songid in songlists.master_list:
                     print(App.Songs[songid][0])
-            print()
-                
-            #check to see if song_S can be placed in the masterlist
-            #if so, place it. (other checks also take place)
-            masterlist.check(song_S, songbook)
+            elif better == "Save":
+                filename = input("Enter filename: ")
+                songlists.savedata(filename)
+                quit()
             print()
 
-            if len(masterlist.songs) == len(App.Songs):
+            # check to see if song_I can be placed in the masterlist
+            # if so, place it. (other checks also take place)
+            songlists.check(song_I)
+            print()
+
+            if len(songlists.master_list) == len(App.Songs):
                 break
 
-        for songid in masterlist.songs:
+        for songid in songlists.master_list:
             print(App.Songs[songid])
-        
 
-        
 
 application = App()
 application.execute()
